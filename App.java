3cbfb140808da369fe2119adf7c6b097c4d650c4
leona-27 <100231110+leona-27@users.noreply.github.com>
2022-03-03 09:04:40 +0800

    Add files via upload

---
+++ App.java
@@ -0,0 +1,35 @@
+package Duplicate;
+import java.util.Scanner;
+
+public class App {
+    public static Object[][] item = {{"chubby",20},{"snowbear",15},{"stick o",10},{"kitkat" ,25},{"Tobleron" ,12}};
+    public static void main(String[] args)  {
+        System.out.println("********************************");
+        for (Object[] i: item) {
+            for (Object m: i){
+                System.out.print(m+" \t");
+            }
+            System.out.println();
+        }
+        System.out.println("********************************");
+        System.out.println("What would you like to buy?");
+        Scanner scanner = new Scanner(System.in);
+        String buy = scanner.nextLine();
+        buy.toLowerCase();
+        buyItem(buy);
+    }
+
+    public static void buyItem(String buy){
+        Scanner scanner = new Scanner(System.in);
+        int i = 0;
+        while(!buy.equals(item[i][0])){
+            if(i>item.length){
+                System.out.println("the item you entered does not exist.");
+                System.out.println("what would you like to buy?");
+                buyItem(scanner.nextLine());
+            }
+            i++;
+        }
+        System.out.println(item[i][0]+" is "+item[i][1]+"buy?");
+    }
+}
